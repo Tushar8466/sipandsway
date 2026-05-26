0e9b418ae1960096fe0e062f9b13dc0533c6a788
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-05-26 16:07:31 +0530

    Add Menu and Contact components; update App to include them

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu'
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger , SplitText); // This gsap properties aren't active at all every time so we have to activate them
 
@@ -16,6 +18,8 @@ function App() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
